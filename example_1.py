0dd8437e95338cd1c02fce594612fad67ff013da
BoKyoung Choi <bokyoung9693@gmail.com>
2023-07-13 11:43:30 +0900

    FEAT: Call past data for Samsung Elec

--- example_1.py
+++ example_1.py
@@ -4,7 +4,7 @@ import win32com.client
 # 연결 여부 체크
 objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
 bConnect = objCpCybos.IsConnect
-if (bConnect != 0):
+if (bConnect == 0):
     print("PLUS가 정상적으로 연결되지 않음. ")
     exit()
  
